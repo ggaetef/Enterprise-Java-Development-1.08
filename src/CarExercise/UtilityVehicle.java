5ab33e7fbfb5f9e784c06e1e82e103d6de4d112e
gonzafischer <ggaetef@gmail.com>
2022-07-24 19:08:12 +0200

    Primera version

---
+++ src/CarExercise/UtilityVehicle.java
@@ -0,0 +1,26 @@
+package CarExercise;
+
+public class UtilityVehicle extends Car {
+    @Override
+    public void vinNumber() {
+
+    }
+
+    @Override
+    public void make() {
+
+    }
+
+    @Override
+    public void model() {
+
+    }
+
+    @Override
+    public void mileage() {
+
+    }
+
+    boolean fourWheelDrive = true;
+
+}
